5cfd2eae8cf2cc094cdaeb54123a13cf8c310042
GG <georgia@stormbox.com.au>
2024-06-28 16:41:21 +0800

    Added a sign

--- scripts/classes/sign.gd
+++ scripts/classes/sign.gd
@@ -1,13 +1,21 @@
-extends StaticBody2D
+extends Area2D
 
-@export var text: String
-@onready var label = $Control/RichTextLabel
+@export var description: String
 
+@onready var label = $Control/TextureRect/Label
+@onready var tooltip = $Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	label.text = str(description)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_body_entered(body):
+	if body is Player:
+		print("Enter")
+		tooltip.visible = true
+
+
+func _on_body_exited(body):
+	if body is Player:
+		print("Exit")
+		tooltip.visible = false
